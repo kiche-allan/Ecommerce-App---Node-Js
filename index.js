28c56f65710c70374dd73f0c6f6cdb3539ff289f
Allan Kiche <tle2819@students.mu.ac.ke>
2023-04-17 23:11:18 +0300

    ecommerce2

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 4000;
+app.use('/', (req, res) => {
+    res.send("hello from server side")
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running at PORT ${PORT}`)
-})
+});
